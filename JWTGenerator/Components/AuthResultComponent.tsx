9421368c133991e0f8873c2661b34c78acc5f0ae
Imran <imran.loves.to.code@gmail.com>
2021-12-19 15:31:57 +0000

    added code component and copy functionality

--- JWTGenerator/Components/AuthResultComponent.tsx
+++ JWTGenerator/Components/AuthResultComponent.tsx
@@ -1,19 +1,34 @@
 import * as React from 'react';
+import { Button } from 'react-bootstrap';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { tomorrow } from 'react-syntax-highlighter/dist/esm/styles/prism'
+
 
 interface Props {
     authResult : string
 }
 
-const textAreaStyle = {
-    textAlign: "left"    
+const textAreaStyle = {    
+    top: "1rem",
+    right: "1rem",
+    zIndex: "10",
+    display: "block",
+    padding: ".25rem .5rem",    
+    color: "#0d6efd",
+    backgroundColor: "#fff",
+    border: "1px solid",
+    borderRadius: ".25rem"
 };
+
+const t = () => {
+    navigator.clipboard.writeText('this is for copy')
+}
 
 const AuthResultComponent = (props: Props) => {
     return (
         <div className='row text-start'>
-                <div className='col-12'>
+                <div className='col-12 position-relative'>                    
+                    <Button className="position-absolute" style={textAreaStyle}>hi</Button>
                     
                 <SyntaxHighlighter language="json" style={tomorrow} wrapLongLines wrapLines>
                     {props.authResult}
